a131d9f9a22dbf51e7c322460fdf46944e39dec1
equin0x <nope@equin0x.com>
2012-07-31 15:34:23 -0400

    Simple function for calling all files in a folder.

---
+++ CALLF.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+IF "%1"=="" goto False
+IF NOT "%1"=="" goto True1
+:True1
+IF EXIST %1 goto True2
+IF NOT EXIST %1 goto False
+:True2
+ECHO Calling all files from the folder: %1
+for /f %%A in ('dir /b /s %1\*.bat') do (
+	call %%A
+)
+for /f %%B in ('dir /b /s %1\*.cmd') do (
+	call %%B
+)
+GoTo EOF
+
+:False
+ECHO No folder supplied/Invalid folder.
+GoTo EOF
+
+:EOF
